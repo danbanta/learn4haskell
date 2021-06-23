bd9c2cf538d6aa839f371d6208bd8a2c3a395e4a
Dan Banta <danbanta@users.noreply.github.com>
2021-06-23 14:11:41 -0400

    Update src/Chapter1.hs
    Co-authored-by: Dmitrii Kovanikov <kovanikov@gmail.com>

--- src/Chapter1.hs
+++ src/Chapter1.hs
@@ -490,7 +490,7 @@ Implement a function that returns the last digit of a given number.
   whether it works for you!
 -}
 lastDigit :: Integral a => a -> a
-lastDigit n = mod (abs (n)) 10
+lastDigit n = mod (abs n) 10
 
 
 {- |
